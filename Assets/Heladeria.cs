f723f440e6a9c64b1f780bef8c06afd47e8b49db
joaquinmora14 <joacomora13@gmail.com>
2025-04-11 10:13:19 -0300

    sin terminar probando

---
+++ Assets/Heladeria.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heladeria : MonoBehaviour
+{
+    public string gustoDeHelado;
+    public float cantidadDeHelado;
+    float precioPorGramo = 1.25f;
+    int cantidadMinima = 250;
+    int cantidadMaxima = 3000;
+    float precioFinal;
+    float descuentoFru = 0.9;
+    float precioFinalDescuento;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gustoDeHelado = "DDL" ||gustoDeHelado = "CHO") ;
+        precioFinal = cantidadDeHelado * precioPorGramo;
+        Debug.Log("El precio final es " + precioFinal);
+
+        if (cantidadDeHelado < cantidadMinima || cantidadDeHelado > cantidadMaxima) ;
+        Debug.log("La cantidad ingresada no es valida, el minimo es 250 y el maximo es 3000");
+
+        if (gustoDeHelado = "FRU") ;
+        precioFinal = cantidadDeHelado * precioPorGramo;
+        precioFinalDescuento = precioFinal * descuentoFru;
+        //precioFinal* descuentoFru = precioFinalDescuento;
+
+        Debug.Log("El precio final es " + precioFinalDescuento);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
