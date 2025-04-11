3a7844e2327a0808544171906ce9765cbdb16eb9
joaquinmora14 <joacomora13@gmail.com>
2025-04-11 10:35:20 -0300

    TERMINADO

--- Assets/Heladeria.cs
+++ Assets/Heladeria.cs
@@ -5,29 +5,40 @@ using UnityEngine;
 public class Heladeria : MonoBehaviour
 {
     public string gustoDeHelado;
-    public float cantidadDeHelado;
+    public int cantidadDeHelado;
     float precioPorGramo = 1.25f;
     int cantidadMinima = 250;
     int cantidadMaxima = 3000;
     float precioFinal;
-    float descuentoFru = 0.9;
-    float precioFinalDescuento;
+    float descuentoFru = 0.9F;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (gustoDeHelado = "DDL" ||gustoDeHelado = "CHO") ;
-        precioFinal = cantidadDeHelado * precioPorGramo;
-        Debug.Log("El precio final es " + precioFinal);
+        if (cantidadDeHelado < cantidadMinima || cantidadDeHelado > cantidadMaxima)
+        {
+            Debug.Log("La cantidad ingresada no es valida, el minimo es 250 y el maximo es 3000");
+            return;
+        }
+        else if (gustoDeHelado != "FRU" && gustoDeHelado != "CHO" && gustoDeHelado != "DDL")
+        { 
+        Debug.Log("Codigo no valido");
+        return;
+        }
 
-        if (cantidadDeHelado < cantidadMinima || cantidadDeHelado > cantidadMaxima) ;
-        Debug.log("La cantidad ingresada no es valida, el minimo es 250 y el maximo es 3000");
+        if (gustoDeHelado == "DDL" || gustoDeHelado == "CHO")
+        {
+            precioFinal = cantidadDeHelado * precioPorGramo;
+            Debug.Log("El precio final es " + precioFinal);
+        }
+        else if(gustoDeHelado == "FRU")
+        {
+            precioFinal = cantidadDeHelado * precioPorGramo;
+            precioFinal *= descuentoFru;
+            Debug.Log("El precio final es " + precioFinal);
+        }
 
-        if (gustoDeHelado = "FRU") ;
-        precioFinal = cantidadDeHelado * precioPorGramo;
-        precioFinalDescuento = precioFinal * descuentoFru;
-        //precioFinal* descuentoFru = precioFinalDescuento;
 
-        Debug.Log("El precio final es " + precioFinalDescuento);
 
 
     }
